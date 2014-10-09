71581e1ec2a7486112c711f9be5139a5f62b9400
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2014-10-09 11:59:31 -0500

    WIP: Trash: comment out the registration printing

--- BRAINSABC/brainseg/BRAINSABC.cxx
+++ BRAINSABC/brainseg/BRAINSABC.cxx
@@ -1048,6 +1048,7 @@ int main(int argc, char * *argv)
 
   atlasToSubjectPreSegmentationTransform =
     atlasreg->GetAtlasToSubjectTransform();
+  /*
   if( debuglevel > 9 )
     { // NOTE THIS IS REALLY ANNOYING FOR LARGE BSPLINE REGISTRATIONS!
     muLogMacro( << __FILE__ << " " << __LINE__ << " "
@@ -1055,6 +1056,7 @@ int main(int argc, char * *argv)
     muLogMacro( << __FILE__ << " " << __LINE__ << " "
                 << atlasToSubjectPreSegmentationTransform->GetParameters() << std::endl );
     }
+  */
   if( debuglevel > 4 )
     {
     // Write the registered template and images
