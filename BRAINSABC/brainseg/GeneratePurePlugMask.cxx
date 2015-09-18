b807564f3346468727fb7480c4fce88159d94a11
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2015-09-18 18:55:27 -0500

    WIP: use sample object

--- BRAINSABC/brainseg/GeneratePurePlugMask.cxx
+++ BRAINSABC/brainseg/GeneratePurePlugMask.cxx
@@ -15,13 +15,52 @@
 int main( int argc, char * argv[] )
 {
 
-  // create a sample test matrix
+  // create a sample test ListSample object
+  const unsigned int MeasurementVectorLength = 8;
+  typedef itk::Vector< float, MeasurementVectorLength > MeasurementVectorType;
+  typedef itk::Statistics::ListSample< MeasurementVectorType > SampleType;
+  SampleType::Pointer sample = SampleType::New();
+  sample->SetMeasurementVectorSize( MeasurementVectorLength );
+  MeasurementVectorType mv;
+  mv[0] = 0;
+  mv[1] = 0;
+  mv[2] = 3;
+  mv[3] = -3;
+  mv[4] = 3;
+  mv[5] = -3;
+  mv[6] = 3;
+  mv[7] = -3;
+  mv[8] = 0;
+  sample->PushBack( mv );
+
+  mv[0] = 1;
+  mv[1] = -1;
+  mv[2] = 1;
+  mv[3] = 1;
+  mv[4] = -1;
+  mv[5] = -1;
+  mv[6] = 0;
+  mv[7] = 0;
+  mv[8] = 3;
+  sample->PushBack( mv );
+
+  // compute mean and covariance
+  typedef itk::Statistics::CovarianceSampleFilter< SampleType > CovarianceAlgorithmType;
+  CovarianceAlgorithmType::Pointer covarianceAlgorithm = CovarianceAlgorithmType::New();
+  covarianceAlgorithm->SetInput( sample );
+  covarianceAlgorithm->Update();
+
+  typename CovarianceAlgorithmType::MeasurementVectorRealType mean = covarianceAlgorithm->GetMean();
+  typename CovarianceAlgorithmType::MatrixType cov = covarianceAlgorithm->GetCovarianceMatrix();
+
+
+
   // compute mean
   // compute covariance
   // compute MD and ED
   // create Dist
   // return isPure!
-
+/*
   vnl_matrix<double> measurementMatrix(2,8);
   measurementMatrix.fill(1);
 
@@ -31,7 +70,7 @@ int main( int argc, char * argv[] )
   {
   mean[c] = measurementMatrix.get_row(c).mean();
   }
-
+*/
 
 
 
